6ebf3e59c273a6ccaa86001c74792a347d29bfda
Kumarsb22 <kumarsb020@gmail.com>
2025-02-28 18:13:42 +0530

    config server

--- MS_GREET_API/MS_GREET_API/src/main/java/com/example/msgreetapi/controller/GreetRestController.java
+++ MS_GREET_API/MS_GREET_API/src/main/java/com/example/msgreetapi/controller/GreetRestController.java
@@ -1,24 +1,30 @@
 package com.example.msgreetapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.msgreetapi.service.WelcomeFienClientService;
 
+@RefreshScope
 @RestController
 public class GreetRestController {
 	private Environment env;
+
+	@Value("${msg}")
+	private String msg;
 
 	@Autowired
 	private WelcomeFienClientService clientService;
 
 	@GetMapping("/greet")
 	public String greetMsg() {
-	//String port=env.getProperty("server.port");
-		String msg = "Good Afternoon ";
+		// String port=env.getProperty("server.port");
+		String msg1 = msg;
 		String welcomeMsg = this.clientService.invokeWelcomeApi();
-		return msg.concat(welcomeMsg);
+		return msg1.concat(welcomeMsg);
 	}
 }
